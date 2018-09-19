fe5838ce17274ffbc8f67ffade9b907755342ae7
GarryDev <garry.mason.d@gmail.com>
2018-09-19 20:14:32 +0100

    Centered tiles with changible offset.

--- main.py
+++ main.py
@@ -3,43 +3,45 @@ import resources as res
 import traceback
 import sys
 import tile
-
 import chunk_gen
+
+WINDOW_WIDTH    = 800
+WINDOW_HEIGHT   = 600
+
+chunk_size = 32
+chunk_span = chunk_size//4
+
+# window subclass
+class MainWindow(pyglet.window.Window):
+    def __init__(self):
+        pyglet.window.Window.__init__(self)
+        self.set_size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.set_caption('Window subclass')
+
+window = MainWindow()
 
 try:
     res.initialize()
 except Exception as e:
     sys.exit(traceback.format_exc())
 
-chunk_size = 32
-chunk_span = 8
+x_offset = 0
+y_offset = 0
+x_center = (WINDOW_WIDTH //2) - ((chunk_span//2) * tile.WIDTH)
+y_center = (WINDOW_HEIGHT//2) - ((chunk_span//2) * tile.HEIGHT)
+
 chunk = chunk_gen.create_chunk(chunk_gen.TerrainType.FOREST, chunk_size)
 
 main_batch = pyglet.graphics.Batch()
 sprites = []
 
-# for x in range(0, tile.WIDTH):
-#     for y in range(0, tile.HEIGHT):
-#         sprites.append(pyglet.sprite.Sprite(img = res.sprite_grid[chunk[(x*y) + x].tile_id], x = tile.WIDTH * x, y = tile.WIDTH * y, batch=main_batch))
-
-# for x in range(0, 5):
-#     for y in range(0, 5):
-#         sprites.append(pyglet.sprite.Sprite(img = res.sprite_grid[chunk[(x*y) + x].tile_id], x = tile.WIDTH * x, y = tile.WIDTH * y, batch=main_batch))
-
-size = tile.WIDTH
-
 for y in range(0, chunk_span):
     for x in range(0, chunk_span):
-        sprites.append(pyglet.sprite.Sprite(img = res.sprite_grid[chunk[(y * chunk_span) + x].id], x = x * size, y = y * size, batch=main_batch))
-
-# window subclass
-class MainWindow(pyglet.window.Window):
-    def __init__(self):
-        pyglet.window.Window.__init__(self)
-        self.set_size(800, 600)
-        self.set_caption('Window subclass')
-
-window = MainWindow()
+        sprites.append(pyglet.sprite.Sprite(
+            img = res.sprite_grid[chunk[(y * chunk_span) + x].id],
+            x = x_center + x_offset + (y * tile.WIDTH),
+            y = y_center + y_offset + (x * tile.WIDTH),
+            batch=main_batch))
 
 #batch = pyglet.graphics.Batch()
 #background = pyglet.graphics.OrderedGroup(0)
--- tile.py
+++ tile.py
@@ -1,23 +1,23 @@
-from enum import Enum, auto
-from random import choice
+#from enum import Enum, auto
+#from random import choice
 
 WIDTH = 32
 HEIGHT = 32
 
-class Direction(Enum):
-    North = auto()
-    East = auto()
-    West = auto()
-    South = auto()
+# class Direction(Enum):
+#     North = auto()
+#     East = auto()
+#     West = auto()
+#     South = auto()
 
-    @classmethod
-    def rand(cls):
-        return choice(list(cls.__members__.values()))
+#     @classmethod
+#     def rand(cls):
+#         return choice(list(cls.__members__.values()))
 
-    def __repr__(self):
-        return '<%s.%s>' % (self.__class__.__name__, self.name)
+#     def __repr__(self):
+#         return '<%s.%s>' % (self.__class__.__name__, self.name)
 
 class Tile:
     def __init__(self, id):
         self.id = id
-        self._dir = Direction.rand()
+#       self._dir = Direction.rand()
